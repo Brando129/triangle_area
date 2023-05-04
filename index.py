58a337a16ffc8a8ae3ba960e49b565d0c34e964d
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-04 10:21:23 -0400

    Added f string to return statement

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ def find_area(base, height):
     base = base/2
     height = height
     area = int(base * height)
-    return area
+
+    return f"The area of your triangle is {area}"
 
 print(find_area(12, 5))
